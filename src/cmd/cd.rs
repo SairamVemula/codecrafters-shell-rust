9ede37a4e75c1c540b1b1d61d907cdf848a12ea6
Sairam Vemula <sairamvemula1998@gmail.com>
2026-05-08 19:38:31 +0530

    codecrafters submit [skip ci]

--- src/cmd/cd.rs
+++ src/cmd/cd.rs
@@ -4,7 +4,7 @@ pub fn handle_cd(args: Vec<&str>) {
     let path = Path::new(args[0]);
 
     if let Err(_) = env::set_current_dir(path) {
-        println!("cd: {}: : No such file or directory", path.display());
+        println!("cd: {}: No such file or directory", path.display());
 
     }
 }
