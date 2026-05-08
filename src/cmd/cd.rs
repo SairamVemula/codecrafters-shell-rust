6dc213ab1c0a8152fd331b152a2ecd45850c7a14
Sairam Vemula <sairamvemula1998@gmail.com>
2026-05-08 19:44:47 +0530

    codecrafters submit [skip ci]

--- src/cmd/cd.rs
+++ src/cmd/cd.rs
@@ -1,10 +1,35 @@
-use std::{env, path::Path};
+use std::{
+    env,
+    path::{ PathBuf},
+};
 
 pub fn handle_cd(args: Vec<&str>) {
-    let path = Path::new(args[0]);
+    let path = args.get(0).unwrap_or(&"~");
 
-    if let Err(_) = env::set_current_dir(path) {
-        println!("cd: {}: No such file or directory", path.display());
+    let expanded = expand_path(path);
 
+    if let Err(_) = env::set_current_dir(&expanded) {
+        println!("cd: {}: No such file or directory", expanded.display());
     }
+}
+
+fn expand_path(input: &str) -> PathBuf {
+    if input == "~" {
+        return env::var("HOME")
+            .or_else(|_| env::var("USERPROFILE"))
+            .map(PathBuf::from)
+            .unwrap_or_else(|_| PathBuf::from("/"));
+    }
+
+    if let Some(stripped) = input.strip_prefix("~/") {
+        let mut home = env::var("HOME")
+            .or_else(|_| env::var("USERPROFILE"))
+            .map(PathBuf::from)
+            .unwrap_or_else(|_| PathBuf::from("/"));
+
+        home.push(stripped);
+        return home;
+    }
+
+    PathBuf::from(input)
 }
